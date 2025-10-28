c2889eccf4bdac4dc61b2af09a4095edb78afd42
Madhurmehare27 <117211050+MADHURMEHARE@users.noreply.github.com>
2025-10-29 00:18:31 +0530

    learn module import and export comman method  arrow fucntionpractice

---
+++ Module/understandingmodule/import.js
@@ -0,0 +1,5 @@
+const sum = require('./sum.js');
+
+console.log("Imported Sum is:",sum.a + sum.b);
+ const mul =sum.mult(5,4);
+ console.log("Multiplication is:",mul);
---
+++ Module/understandingmodule/sum.js
@@ -0,0 +1,13 @@
+export const a=10;
+export const b=20;
+
+console.log("Sum is:", a + b);
+
+export function div (x, y) {
+    return x + y;
+}
+
+export function mult(x,y)
+{
+    return x*y ;
+}
